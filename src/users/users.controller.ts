b6614d5dc085b536cd116d747cbaf916777e5a24
Luqman Rasat <luqmanrasat@hotmail.com>
2022-07-25 02:09:57 +0800

    add function return type

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,7 @@ import {
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -18,21 +19,23 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('signup')
-  createUser(@Body() createUserDto: CreateUserDto) {
+  createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
 
   @Get(':id')
-  async findUser(@Param('id', ParseIntPipe) id: number) {
+  async findUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
     const user = this.usersService.findOne(id);
 
     if (!user) {
       throw new NotFoundException('User not found!');
     }
+
+    return user;
   }
 
   @Get()
-  findUsers() {
+  findUsers(): Promise<User[]> {
     return this.usersService.find();
   }
 
@@ -40,12 +43,12 @@ export class UsersController {
   updateUser(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
-  ) {
+  ): Promise<User> {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  deleteUser(@Param('id', ParseIntPipe) id: number) {
+  deleteUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
     return this.usersService.remove(id);
   }
 }
