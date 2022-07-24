5ea37460b79f0f789361a00343d7a4b496dd6db3
Luqman Rasat <luqmanrasat@hotmail.com>
2022-07-25 01:29:40 +0800

    use typeorm hooks to log

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +17,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log(`User #${this.id} is inserted.`);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log(`User #${this.id} is updated.`);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log(`User #${this.id} is removed.`);
+  }
 }
