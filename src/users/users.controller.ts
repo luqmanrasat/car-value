edd2c33e77eca7bc8f6570ae4eaaf5d49c42b977
Luqman Rasat <luqmanrasat@hotmail.com>
2022-07-25 02:08:33 +0800

    throw exception if user not found

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
   ParseIntPipe,
   Patch,
@@ -22,8 +23,12 @@ export class UsersController {
   }
 
   @Get(':id')
-  findUser(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.findOne(id);
+  async findUser(@Param('id', ParseIntPipe) id: number) {
+    const user = this.usersService.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
   }
 
   @Get()
