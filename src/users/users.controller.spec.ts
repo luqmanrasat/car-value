21512302b9396ef491c4a681636957426dba8e9d
Luqman Rasat <luqmanrasat@hotmail.com>
2022-07-28 04:44:35 +0800

    add users controller test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -8,46 +8,57 @@ import { UsersService } from './services/users.service';
 import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
-  const mockUser = {
+  let controller: UsersController;
+  let users: User[];
+  let mockSession: { userId: number };
+
+  const mockAuthUserDto = {
     email: 'test@test.com',
     password: 'test123',
   };
 
-  const mockSession = {
-    userId: null,
-  }
-
   const mockUsersService = {
-    findOneById: jest.fn((id: number) =>
-      Promise.resolve({ ...mockUser, id } as User),
-    ),
+    findOneById: jest.fn((id: number) => {
+      const [user] = users.filter((user) => user.id === id);
+      return Promise.resolve(user);
+    }),
     find: jest.fn(() => Promise.resolve(users)),
-    update: jest.fn((id: number, updateUserDto: UpdateUserDto) =>
-      Promise.resolve({
-        id,
-        ...mockUser,
-        ...updateUserDto,
-      } as User),
-    ),
-    remove: jest.fn((id: number) =>
-      Promise.resolve({
-        id,
-        ...mockUser,
-      } as User),
-    ),
+    update: jest.fn((id: number, updateUserDto: UpdateUserDto) => {
+      let updatedUser: User;
+      for (const user of users) {
+        if (user.id === id) {
+          for (const key in updateUserDto) {
+            user[key] = updateUserDto[key];
+          }
+          updatedUser = user;
+          break;
+        }
+      }
+      return Promise.resolve(updatedUser);
+    }),
+    remove: jest.fn((id: number) => {
+      let removedUser: User;
+      for (const index in users) {
+        if (users[index].id === id) {
+          [removedUser] = users.splice(+index, 1);
+          break;
+        }
+      }
+      Promise.resolve(removedUser);
+    }),
   };
 
   const mockAuthService = {
     signup: jest.fn((authUserDto: AuthUserDto) => {
-      const user = authUserDto as User;
+      const user = { id: new Date().valueOf(), ...authUserDto } as User;
       users.push(user);
       return Promise.resolve(user);
     }),
-    signin: jest.fn(),
+    signin: jest.fn((authUserDto: AuthUserDto) => {
+      const [user] = users.filter((user) => user.email === authUserDto.email);
+      return Promise.resolve(user);
+    }),
   };
-  
-  let controller: UsersController;
-  let users: User[] = [];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -59,30 +70,76 @@ describe('UsersController', () => {
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    users = [];
+    mockSession = { userId: null };
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
+  describe('createUser()', () => {
+    it('should save created user and return it', async () => {
+      const user = await controller.createUser(mockAuthUserDto, mockSession);
+      expect(user).toBeDefined();
+      expect(users).toContainEqual(user);
+    });
+  });
+
+  describe('signin()', () => {
+    it('should save userId in session object', async () => {
+      await controller.createUser(mockAuthUserDto, mockSession);
+      const user = await controller.signin(mockAuthUserDto, mockSession);
+      expect(mockSession.userId).toEqual(user.id);
+    });
+  });
+
+  describe('signout()', () => {
+    it('should clear userId in session object', async () => {
+      await controller.createUser(mockAuthUserDto, mockSession);
+      await controller.signin(mockAuthUserDto, mockSession);
+      controller.signOut(mockSession);
+      expect(mockSession.userId).toBeNull();
+    });
+  });
+
   describe('findUsers()', () => {
     it('should return an array of users', async () => {
-      await controller.createUser(mockUser, mockSession);
+      await controller.createUser(mockAuthUserDto, mockSession);
       const result = await controller.findUsers();
       expect(result.length).toEqual(1);
-    })
+    });
   });
 
   describe('findUser()', () => {
     it('should throw a NotFoundException if user does not exist', async () => {
-      mockUsersService.findOneById.mockResolvedValueOnce(null);
-      await expect(controller.findUser(1)).rejects.toThrowError(NotFoundException);
-    })
+      await expect(controller.findUser(420)).rejects.toThrowError(
+        NotFoundException,
+      );
+    });
 
     it('should return user with the given id', async () => {
-      await controller.createUser(mockUser, mockSession);
-      const result = await controller.findUser(420);
-      expect(result.id).toEqual(420);
-    })
+      const user = await controller.createUser(mockAuthUserDto, mockSession);
+      const result = await controller.findUser(user.id);
+      expect(result.id).toEqual(user.id);
+    });
+  });
+
+  describe('updateUser()', () => {
+    it('should update the selected user with new value given', async () => {
+      const user = await controller.createUser(mockAuthUserDto, mockSession);
+      const result = await controller.updateUser(user.id, {
+        email: 'newemail@test.com',
+      });
+      expect(users).toContainEqual(result);
+    });
+  });
+
+  describe('deleteUser()', () => {
+    it('should remove the selected user', async () => {
+      const user = await controller.createUser(mockAuthUserDto, mockSession);
+      await controller.deleteUser(user.id);
+      expect(users).not.toContainEqual(user);
+    });
   });
 });
